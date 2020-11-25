323d3a4267648abf16439ea84c288bd584be1651
SuJinTuan <sujin810969050@qq.com>
2020-11-25 20:17:51 +0800

    '初始化项目'

--- src/router.js
+++ src/router.js
@@ -3,8 +3,8 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 
 Vue.use(Router);
-
-const BaseMessage =  () => import(/* webpackChunkName: "baseMsg" */ "./components/BaseMessage.vue")
+// 实现懒加载
+const BaseMessage = () => import(/* webpackChunkName: "baseMsg" */ "./components/BaseMessage.vue")
 const PersonalSkill = () => import( /* webpackChunkName: "skillMsg" */ "./components/PersonalSkill.vue")
 const InternExperience = () => import( /* webpackChunkName: "internMsg" */ "./components/InternExperience.vue")
 const EducationExperience = () => import( /* webpackChunkName: "educationMsg" */ "./components/EducationExperience.vue")
@@ -19,10 +19,11 @@ export default new Router({
       name: "home",
       redirect: '/base',
       component: Home,
+      // 子路由
       children: [
         { path: "/base", name: 'base', component: BaseMessage },
         { path: "/skill", name: 'skill', component: PersonalSkill },
-        { path: "/intern",name: 'intern',  component: InternExperience },
+        { path: "/intern", name: 'intern', component: InternExperience },
         { path: "/education", name: "education", component: EducationExperience },
         { path: "/project", name: "project", component: ProjectExperience }
       ]
@@ -30,9 +31,6 @@ export default new Router({
     {
       path: "/preview",
       name: "preview",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "showEdit" */ "./views/ShowEdit.vue")
     }
